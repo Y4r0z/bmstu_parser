e9a6cd77b4b0d5d10f81b273fc88dbb63862f05c
Yaroslav Bogdanov <jarosl.s@yandex.ru>
2023-05-11 17:38:11 +0300

    Исправление таймера

--- debug/timer.py
+++ debug/timer.py
@@ -3,7 +3,8 @@ import time
 import logging as log
 
 class Timer:
-    def __init__(self) -> None:
+    def __init__(self, name = None) -> None:
+        self.name = name
         self.start = time.time()
         self.current = self.start
         self.marks = []
@@ -18,7 +19,7 @@ class Timer:
         self.marks.append(dif)
     
     def print(self):
-        print("------ Timer ------\n" +
+        print(f"------ Timer {self.name}------\n" +
         f"Marks count: {len(self.marks)};\n" +
         "--- Marks:")
         for i in self.marks:
